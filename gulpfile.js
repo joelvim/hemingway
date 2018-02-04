79fa3a59ceccb6a74dbca1dd9d2811ff1ccc640a
Joël Vimenet <joel.vimenet@gmail.com>
2018-02-04 14:09:45 +0100

    Add subtitle support
    Change build system
    Change markup

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,32 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var clean = require("gulp-clean");
 
-gulp.task("default", function () {
-  gulp.src('sass/style.sass')
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('static/css/'));
+var callSass = function(prod) {
+  if (prod) {
+    return sass({outputStyle: "compressed"});
+  } else {
+    return sass();
+  }
+};
+
+const produceCss = function(prod) {
+  return function() {
+    gulp
+      .src("sass/style.sass")
+      .pipe(callSass(prod))
+      .pipe(autoprefixer())
+      .pipe(gulp.dest("static/css/"));
+  };
+};
+
+gulp.task("default", produceCss(true));
+
+gulp.task("watch", function() {
+  gulp.watch("sass/style.sass", produceCss(false));
 });
 
-gulp.task("watch", function () {
-  gulp.watch('sass/style.sass', function() {
-    gulp.src('sass/style.sass')
-      .pipe(sass({outputStyle: 'compressed'}))
-      .pipe(autoprefixer())
-      .pipe(gulp.dest('static/css/'));
-  });
+gulp.task("clean", function() {
+  gulp.src("static/css").pipe(clean());
 });
